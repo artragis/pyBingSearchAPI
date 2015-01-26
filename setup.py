a1a030d71f8bea632c7e0c381cd46d23e9387603
artragis <artragis@gmail.com>
2015-01-26 18:11:25 +0100

    full pypi support

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from distutils.core import setup
 from setuptools import find_packages
-import bing_search_api
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
 setup(
     name='pyBingSearchAPI',
     version='1',
-    packages=find_packages,
+	install_requires=['requests'],
     url='https://github.com/artragis/pyBingSearchAPI',
     license='GPLv2',
     author='',
     author_email='',
     description='pyBingSearchAPI is a connector for Bing Search API for version 2.0 and later',
-    install_requires=REQUIREMENTS,
     long_description=open('README.md').read()
 )
